58651734f070a505083ee67009ce74b578636e93
zhengmaoshun <2822734904@qq.com>
2021-06-22 10:47:20 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  title: '个人博客', //左上角头部导航条的标题
+  base:'/myBlog/',//gitHub仓库基础路径
+  dest: 'docs/dist', /* 打包文件基础路径, 在命令所在目录下 */
+  description: 'Just playing around', //标题下的描述
+  head: [ //添加浏览器icon图标
+    ['link', { rel: 'icon', href: '/hello.jpg' }]
+  ],
+  plugins: ['@vuepress/back-to-top'],//back-to-top回到顶部
+  themeConfig: { //主题配置
+    nav: [ //头部导航条
+      { text: '首页', link: '/' },
+      { text: '基础教程', link: '/guide/' },
+      { text: '前端笔记', link: '/foo/' },
+      { text: '后端笔记', link: '/google/' },
+      { text: 'github', link: 'https://github.com/zhengmaoshun/myBlog' },
+      // { //下拉列表
+      //   text: 'Languages',
+      //   ariaLabel: 'Language Menu',
+      //   items: [
+      //     { text: 'Chinese', link: '/language/chinese/' },
+      //     { text: 'Japanese', link: '/language/japanese/' }
+      //   ]
+      // },
+      // {//下拉分组
+      //   text: 'Languages',
+      //   items: [
+      //     { 
+      //       text: 'Group1', 
+      //       items: [
+      //         { text: 'html', link: '/language/chinese/' },
+      //         { text: 'css', link: '/language/chinese/' },
+      //       ] 
+      //     },
+      //     { 
+      //       text: 'Group2', 
+      //       items: [
+      //         { text: 'vue', link: '/language/chinese/' },
+      //         { text: 'react', link: '/language/chinese/' },
+      //       ] }
+      //   ]
+      // }
+    ],
+    sidebar:{ //配置侧边栏
+      '/foo/': [
+        '',     /* /foo/ */
+        'one',  /* /foo/one.html */
+        'two'   /* /foo/two.html */
+      ],
+    }
+  },
+  //最后更新时间
+  lastUpdated: 'Last Updated', // string | boolean
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /hello.jpg
+heroText: 科技让世界更美好
+tagline: 分享 协作 创新 共赢
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: 面试题
+  details: 工作日每天搞定一道大厂前端面试题，一年后再回头，会感谢曾经努力的自己！
+- title: 技能点
+  details: 每周重点攻克一个前端知识重难点，我们一起走进高级前端的世界，在进阶之路上，共勉！
+- title: 聊一聊
+  details: 对于面试题或技能点你有什么见解,让我们一起来探讨一下!
+footer: MIT Licensed | Copyright © 2018-present MaoShun Zheng
+---
